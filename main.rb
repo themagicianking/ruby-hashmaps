668bf3a602cb08af14350ffb7b1ccd624c0a0d63
themagicianking <tmoonder@gmail.com>
2024-01-15 18:08:59 -0500

    Initial commit.

---
+++ main.rb
@@ -0,0 +1,34 @@
+raise IndexError if index.negative? || index >= @buckets.length
+
+class HashMap
+
+  def hash(value)
+  end
+
+  def set(key, value)
+  end
+
+  def get(key)
+  end
+
+  def key?(key)
+  end
+
+  def remove(key)
+  end
+
+  def length
+  end
+
+  def clear
+  end
+
+  def keys
+  end
+
+  def values
+  end
+
+  def entries
+  end
+end
